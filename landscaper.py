2ac122495cc2757b5fa646a929edc21c836a3016
Athena Serene <aserenehoosang@gmail.com>
2023-02-01 19:30:18 -0800

    landscaper can now buy scissors

--- landscaper.py
+++ landscaper.py
@@ -1,15 +1,27 @@
 money = 0
-tools = 
+
+tools = [{"name": "scissors", "cost": 5, "profit": 5}, {"name": "push mower", "cost": 25}, {"name": "battery mower", "cost": 250},{"name": "mower team", "cost": 500}]
+
+rate = 1
+
 print("Welcome to Landscaper! Press '1' to Mow the Lawn. Press '2' to check your stats. Press 'Q' to quit the game. Happy mowing!")
 
 while(True):
     
     print(money)
+    if (money >= 5):
+        user_choice = input("You can now buy a pair of rusty scissors for $5! Would you like to buy?")
+        if (user_choice == "y"):
+            rate = 5
+            money -= 5
+            continue
+        else:
+            continue
     user_choice = input("[1] Mow Lawn [2] Check Stats [Q] Quit")
     if (user_choice == "q"):
         break
     elif (user_choice == "1"):
-        money += 1
+        money += rate
         print("You mowed the lawn! You earned $1.")
         continue
     elif (user_choice == "2"):
