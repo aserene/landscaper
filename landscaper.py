10cdf404aee6fed09edd3db90e8201fdecda24b1
Athena Serene <aserenehoosang@gmail.com>
2023-02-01 19:00:54 -0800

    landscaper can use teeth to cut grass

--- landscaper.py
+++ landscaper.py
@@ -1,7 +1,9 @@
+money = 0
+tools = 
+print("Welcome to Landscaper! Press '1' to Mow the Lawn. Press '2' to check your stats. Press 'Q' to quit the game. Happy mowing!")
+
 while(True):
-    print("Welcome to Landscaper! Press '1' to Mow the Lawn. Press '2' to check your stats. Press 'Q' to quit the game. Happy mowing!")
-    money = 0
-    restart = True
+    
     print(money)
     user_choice = input("[1] Mow Lawn [2] Check Stats [Q] Quit")
     if (user_choice == "q"):
@@ -9,14 +11,14 @@ while(True):
     elif (user_choice == "1"):
         money += 1
         print("You mowed the lawn! You earned $1.")
-        restart
+        continue
     elif (user_choice == "2"):
         if (money == 0):
             print("You don't have any money....")
-            restart
+            continue
         else :
             print(f"You have earned ${money}!")
-            restart
+            continue
     else:
         print("Please make another selection.")
-        restart
+        continue
